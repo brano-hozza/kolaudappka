af9b92db0b4a58d623bd604772ccf498ad7c0cbe
Lucia Mandová <lucka.mandova@gmail.com>
2023-10-02 19:40:22 +0200

    :sparkles: add pngs and titles

--- components/CircleImageButton.vue
+++ components/CircleImageButton.vue
@@ -1,42 +1,24 @@
 <template>
     <div>
-        <div class="circle-image-button">
-            <img :src="props.imageUrl" alt="image" @click="changeRoute" />
+        <div
+            class="w-80 h-80 rounded-full bg-black flex justify-center items-center mb-4"
+        >
+            <img
+                class="mx-auto"
+                :src="imageUrl"
+                alt="image"
+                @click="$emit('click')"
+            />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-const router = useRouter()
-const props = defineProps({
+defineEmits(['click'])
+defineProps({
     imageUrl: {
         type: String,
         required: true,
     },
-    pathUrl: {
-        type: String,
-        required: true,
-    },
 })
-const changeRoute = () => {
-    router.push(props.pathUrl)
-}
 </script>
-<style lang="scss" scoped>
-.circle-image-button {
-    width: 20rem;
-    height: 20rem;
-    border-radius: 50%;
-    background-color: #000000;
-    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 1rem;
-    cursor: pointer;
-    img {
-        width: 200%;
-        height: 200%;
-    }
-}
-</style>
