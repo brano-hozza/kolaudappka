39c3b8be2d9826536a157816ccc30eb82b4992cf
Branislav Hozza <brankohozza@gmail.com>
2023-10-03 22:40:27 +0200

    :sparkles: better

--- server/api/snack-status/[id].put.ts
+++ server/api/snack-status/[id].put.ts
@@ -2,7 +2,7 @@ import { useSnackService } from '~/server/services'
 
 export default defineEventHandler(async (event) => {
     const id = getRouterParam(event, 'id')
-    if (!id || isNaN(Number(id))) {
+    if (id === undefined || isNaN(Number(id))) {
         throw createError({
             statusText: 'Bad Request',
             statusCode: 400,
