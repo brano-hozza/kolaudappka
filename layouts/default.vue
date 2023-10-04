f9e111be5d7dbcc73fcb8c9e3109506215e267f3
Lucia Mandová <lucka.mandova@gmail.com>
2023-10-04 21:29:07 +0200

    :recycle: better nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,26 @@
 <template>
-    <div class="h-fit w-full flex flex-col items-center">
-        <nav
-            class="w-full flex justify-between items-center px-4 py-2 bg-black shadow-md"
-        >
-            <NuxtLink class="w-1/3" to="/menu">
-                <span
-                    class="flex flex-col items-center justify-center"
-                    :class="{ [iconColor]: true }"
-                >
-                    <Icon size="20%" name="material-symbols:home-outline" />
-                    <p>{{ name }}</p>
-                </span>
-            </NuxtLink>
-            <button class="w-1/3" @click="logout">
-                <span class="flex flex-col items-center justify-center">
-                    <Icon
-                        size="20%"
-                        name="fluent:sign-out-20-regular"
-                        color="white"
-                    />
-                    <p class="text-white">Logout</p>
-                </span>
-            </button>
-        </nav>
-        <slot></slot>
-    </div>
+    <nav class="w-full flex py-2 bg-black shadow-md justify-between">
+        <NuxtLink class="w-1/3" to="/menu">
+            <span
+                class="flex flex-col items-center"
+                :class="{ [iconColor]: true }"
+            >
+                <Icon size="20%" name="material-symbols:home-outline" />
+                <p>{{ name }}</p>
+            </span>
+        </NuxtLink>
+        <button class="w-1/3" @click="logout">
+            <span class="flex flex-col items-center">
+                <Icon
+                    size="20%"
+                    name="fluent:sign-out-20-regular"
+                    color="white"
+                />
+                <p class="text-white">Odhlásiť</p>
+            </span>
+        </button>
+    </nav>
+    <slot></slot>
 </template>
 <script lang="ts" setup>
 const name = ref('')
@@ -43,7 +39,7 @@ const route = useRoute()
 const iconColor = computed(() => {
     switch (route.path) {
         case '/menu':
-            return 'text-green-500'
+            return 'text-pinky'
         default:
             return 'text-white'
     }
