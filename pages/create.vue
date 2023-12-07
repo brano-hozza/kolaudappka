f4a085297e14fc1d33a22bf326bdad2d65cc9352
Lucia Mandová <lucka.mandova@gmail.com>
2023-12-07 12:57:06 +0100

    :construction: prepare page for creating party

---
+++ pages/create.vue
@@ -0,0 +1,16 @@
+<!-- First screen - user picks if he wants to create a new party or join existing one -->
+<template>
+    <div class="w-full mt-10 items-center md:flex md:flex-col">
+        <p
+            class="font-basic text-center font-glow-pink text-6xl mb-10 md:mb-0 md:text-8xl"
+        >
+            Nová párty 🎉
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: false,
+})
+</script>
