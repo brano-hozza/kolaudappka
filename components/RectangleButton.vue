7d245204a7201f0955638fcb7f0f09c177577f97
Lucia Mandová <lucka.mandova@gmail.com>
2023-12-07 22:26:18 +0100

    :sparkles: add rectangle button component

---
+++ components/RectangleButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div
+        class="bg-pinky px-10 py-10 rounded-lg text-2xl text-white h-5 mt-10 justify-center items-center flex"
+    >
+        {{ text }}
+    </div>
+</template>
+<script setup lang="ts">
+defineProps({
+    text: {
+        type: String,
+        default: 'default text',
+    },
+})
+</script>
