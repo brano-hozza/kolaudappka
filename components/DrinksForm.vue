caf46d373cbb7baf468b12f54b75f367901c9ea1
Lucia Mandová <lucka.mandova@gmail.com>
2023-12-07 23:22:56 +0100

    :sparkles: drinks form - add new drink logic

--- components/DrinksForm.vue
+++ components/DrinksForm.vue
@@ -6,9 +6,13 @@
         >
             Výber drinkov
         </p>
+        <p class="font-basic font-glow-pink text-7xl my-10 text-center">
+            Koktejly
+        </p>
         <div class="grid grid-cols-1 md:gap-x-10 md:grid-cols-4">
+            <!-- Default cocktails -->
             <div
-                v-for="drink in rawCocktails"
+                v-for="drink in cocktails"
                 :key="drink.type"
                 :ref="(e) => (refs[drink.type] = e as HTMLDivElement)"
                 class="flex flex-col items-center justify-between gap-8 my-4"
@@ -27,9 +31,25 @@
                 <CircleImageButton
                     :image-url="drink.image"
                     :background-color="drink.backgroundColor"
-                    :selected="selectedDrink === drink.type"
+                    :selected="selectedDrinks.includes(drink.type)"
                     size="lg"
                     @click="selectDrink(drink.type)"
+                >
+                </CircleImageButton>
+            </div>
+            <!-- Add drink button -->
+            <div class="flex flex-col items-center justify-between gap-8 my-4">
+                <span class="flex flex-col justify-center items-center gap-4"
+                    ><p class="font-basic text-7xl font-glow-white">
+                        Nový <br />koktejl
+                    </p></span
+                >
+                <CircleImageButton
+                    icon="charm:plus"
+                    icon-size="30%"
+                    size="lg"
+                    background-color="bg-pinky"
+                    @click="addingCocktail = true"
                 >
                 </CircleImageButton>
             </div>
@@ -38,8 +58,9 @@
             Moktejly
         </p>
         <div class="grid grid-cols-1 md:gap-x-10 md:grid-cols-4">
+            <!-- Default mocktails -->
             <div
-                v-for="drink in rawMocktails"
+                v-for="drink in mocktails"
                 :key="drink.type"
                 :ref="(e) => (refs[drink.type] = e as HTMLDivElement)"
                 class="flex flex-col items-center justify-between gap-8 my-4"
@@ -59,19 +80,40 @@
                     :image-url="drink.image"
                     :image-size="drink.imageSize"
                     :background-color="drink.backgroundColor"
-                    :selected="selectedDrink === drink.type"
+                    :selected="selectedDrinks.includes(drink.type)"
                     size="lg"
                     @click="selectDrink(drink.type)"
                 >
                 </CircleImageButton>
             </div>
+            <!-- Add drink button -->
+            <div class="flex flex-col items-center justify-between gap-8 my-4">
+                <span class="flex flex-col justify-center items-center gap-4"
+                    ><p class="font-basic text-7xl font-glow-white">
+                        Nový <br />moktejl
+                    </p></span
+                >
+                <CircleImageButton
+                    icon="charm:plus"
+                    icon-size="30%"
+                    size="lg"
+                    background-color="bg-pinky"
+                    @click="addingMocktail = true"
+                >
+                </CircleImageButton>
+            </div>
         </div>
         <CircleImageButton
+            v-if="selectedDrinks.length"
             floating
-            icon="ri:shopping-basket-2-line"
+            icon="charm:arrow-right"
             size="sm"
             background-color="bg-pinky"
             @click="$emit('click')"
+        />
+        <DrinkFormPopup
+            v-if="addingCocktail || addingMocktail"
+            @add-drink="addDrink"
         />
     </div>
 </template>
@@ -82,9 +124,40 @@ import { DrinkType } from '~/types'
 
 defineEmits(['click'])
 
+const cocktails = ref(rawCocktails)
+const mocktails = ref(rawMocktails)
+
 const refs = ref<Record<string, HTMLDivElement>>({})
-const selectedDrink = ref<DrinkType | null>(null)
+
+const selectedDrinks = ref<DrinkType[]>([])
 const selectDrink = (drinkType: DrinkType) => {
-    selectedDrink.value = drinkType
+    if (selectedDrinks.value.includes(drinkType)) {
+        selectedDrinks.value = selectedDrinks.value.filter(
+            (item) => item !== drinkType
+        )
+    } else {
+        selectedDrinks.value.push(drinkType)
+    }
+}
+
+const addingCocktail = ref(false)
+const addingMocktail = ref(false)
+
+const addDrink = (drinkData: any) => {
+    console.log(drinkData.titles)
+    const newDrink = {
+        type: DrinkType.CustomAlco,
+        // TODO: image: drinkData.imageUrl,
+        image: '/img/drinks/newdrink.png',
+        titles: drinkData.titles,
+        backgroundColor: 'bg-black',
+    }
+    if (addingCocktail) {
+        cocktails.value.push(newDrink)
+        addingCocktail.value = false
+    } else if (addingMocktail) {
+        mocktails.value.push(newDrink)
+        addingMocktail.value = false
+    }
 }
 </script>
