02929346c05fcd9730a6966a5bd73ddd91610965
Lucia Mandová <lucka.mandova@gmail.com>
2023-12-07 18:32:21 +0100

    :sparkles: add styled input

---
+++ components/StyledInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex flex-col w-3/4 mt-5 md:w-1/2">
+        <label class="font-glow-xs-white bold mb-5 text-3xl">{{ label }}</label>
+        <input
+            :type="type"
+            :placeholder="placeholder"
+            class="p-5 rounded-lg text-white bg-gray-900 outline-pink-500"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    type: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+    },
+    placeholder: {
+        type: String,
+        default: '',
+    },
+})
+</script>
