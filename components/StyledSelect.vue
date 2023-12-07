29ee41bef3c75aa5801181cb3000e74742230aa7
Lucia Mandová <lucka.mandova@gmail.com>
2023-12-07 23:21:55 +0100

    :sparkles: add styled select

---
+++ components/StyledSelect.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex flex-col w-3/4 mt-5 md:w-1/2">
+        <label class="font-glow-xs-white bold mb-5 text-3xl">{{ label }}</label>
+        <select class="p-5 rounded-lg text-white bg-gray-900 outline-pink-500">
+            <option v-for="option in options" :key="option" :value="option">
+                {{ option }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineEmits(['change'])
+defineProps({
+    options: {
+        type: Array as PropType<string[]>,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+    },
+})
+</script>
