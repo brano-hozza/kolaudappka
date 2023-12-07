2d77726d101a579a8b0a4e7a3ca7d328a5085a08
Lucia Mandová <lucka.mandova@gmail.com>
2023-12-07 18:33:15 +0100

    :sparkles: add info form

---
+++ pages/create/InfoForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="w-full mt-10 items-center flex flex-col">
+        <!-- TODO: validation -->
+        <p
+            ref="title"
+            class="font-basic text-center font-glow-pink mb-5 text-6xl md:text-8xl"
+        >
+            Nová párty 🎉
+        </p>
+        <StyledInput
+            v-model="organizerName"
+            type="text"
+            label="*Meno organizátora"
+            placeholder="Meno / prezývka organizátora"
+        />
+        <StyledInput
+            v-model="partyName"
+            type="text"
+            label="*Názov párty"
+            placeholder="Názov párty"
+        />
+        <StyledInput
+            v-model="partyDateTime"
+            type="datetime-local"
+            label="*Dátum a čas začiatku"
+        />
+        <StyledInput
+            v-model="evalStartTime"
+            type="time"
+            label="*Spustenie hodnotenia"
+        />
+        <StyledInput
+            v-model="jamLink"
+            type="link"
+            label="Spotify Jam link"
+            placeholder="Vygenerovaný Spotify link"
+        />
+    </div>
+    <div class="mt-10">
+        <CircleImageButton
+            floating
+            icon="charm:arrow-right"
+            size="sm"
+            background-color="bg-pinky"
+            @click="$emit('click')"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+defineEmits(['click'])
+const organizerName = ref('')
+const partyName = ref('')
+const partyDateTime = ref('')
+const evalStartTime = ref('')
+const jamLink = ref('')
+</script>
---
+++ pages/create/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- If current page index is 0, show PartyForm, else show Drinks form -->
+    <div v-if="currentForm === PartyFormType.Info">
+        <InfoForm @click="showNextPage" />
+    </div>
+    <div v-else-if="currentForm === PartyFormType.Drinks">
+        <DrinksForm />
+    </div>
+</template>
+
+<script setup lang="ts">
+import InfoForm from './InfoForm.vue'
+import DrinksForm from './DrinksForm.vue'
+import { PartyFormType } from '@/types'
+definePageMeta({
+    layout: false,
+})
+
+const currentForm = ref(PartyFormType.Info)
+
+const showNextPage = () => {
+    currentForm.value === PartyFormType.Info
+        ? (currentForm.value = PartyFormType.Drinks)
+        : (currentForm.value = PartyFormType.Info)
+}
+</script>
