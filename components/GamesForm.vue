c448456923c1928a30a43fdf7f7a7d03e93b2a25
Lucia Mandová <lucka.mandova@gmail.com>
2023-12-08 02:46:15 +0100

    :sparkles: add games form

---
+++ components/GamesForm.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="w-full mt-10 items-center flex flex-col">
+        <p
+            ref="title"
+            class="font-basic text-center font-glow-pink mb-5 text-6xl md:text-8xl"
+        >
+            Menu hier 🎲
+        </p>
+
+        <div class="grid grid-cols-1 md:gap-x-10 md:grid-cols-4">
+            <!-- Default games -->
+            <div
+                v-for="game in games"
+                :key="game.type"
+                :ref="(e) => (refs[game.type] = e as HTMLDivElement)"
+                class="flex flex-col items-center justify-between"
+            >
+                <CircleImageButton
+                    :image-url="game.image"
+                    :background-color="game.backgroundColor"
+                    :selected="selectedGames.includes(game.type)"
+                    size="lg"
+                    @click="selectGame(game.type)"
+                >
+                </CircleImageButton>
+            </div>
+            <!-- Add new game button -->
+            <CircleImageButton
+                icon="charm:plus"
+                icon-size="30%"
+                size="lg"
+                background-color="bg-pinky"
+                @click="addingGame = true"
+            >
+            </CircleImageButton>
+        </div>
+        <CircleImageButton
+            v-if="selectedGames.length"
+            floating
+            icon="charm:arrow-right"
+            size="sm"
+            background-color="bg-pinky"
+            @click="$emit('nextPage')"
+        />
+        <NewItemModal
+            v-if="addingGame"
+            :is-game="true"
+            title="Nová hra"
+            @add-item="addGame"
+            @close="addingGame = false"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { rawGames } from '~/data/games'
+import { NewItemData, GameType } from '~/types'
+
+defineEmits<{
+    (e: 'addDrink'): void
+    (e: 'close'): void
+    (e: 'nextPage'): void
+}>()
+
+const games = ref(rawGames)
+
+const refs = ref<Record<string, HTMLDivElement>>({})
+
+const selectedGames = ref<GameType[]>([])
+const selectGame = (gameType: GameType) => {
+    if (selectedGames.value.includes(gameType)) {
+        selectedGames.value = selectedGames.value.filter(
+            (item) => item !== gameType
+        )
+    } else {
+        selectedGames.value.push(gameType)
+    }
+}
+
+const addingGame = ref(false)
+
+const addGame = (gameData: NewItemData) => {
+    const newGame = {
+        type: GameType.CustomGame,
+        // TODO: image: drinkData.imageUrl,
+        image: '/img/drinks/newdrink.png',
+        backgroundColor: 'bg-black',
+        // TODO: take from gameData
+        imageSize: '100',
+    }
+    if (addingGame.value) {
+        games.value.push(newGame)
+        addingGame.value = false
+    }
+}
+</script>
