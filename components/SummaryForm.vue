9e8b645b64ea8f5518ea2c0a868f22dfa383c1ea
Lucia Mandová <lucka.mandova@gmail.com>
2023-12-08 03:27:33 +0100

    :sparkles: add summary form

---
+++ components/SummaryForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-full mt-10 items-center flex flex-col">
+        <!-- TODO: validation -->
+        <p
+            class="font-basic text-center font-glow-pink mb-5 text-6xl md:text-8xl"
+        >
+            Zhrnutie
+        </p>
+        <!-- TODO: add summary of all the forms -->
+        <!-- TODO: validate if the code is unique -->
+        <StyledInput
+            v-model="partyCode"
+            type="text"
+            label="*Heslo pre hostí"
+            placeholder="Napr. UZASNA-PARTY"
+        />
+        <RectangleButton
+            text="Vytvoriť párty"
+            @click="$emit('showAdminPage')"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+const partyCode = ref('')
+defineEmits<{
+    (e: 'showAdminPage'): void
+}>()
+</script>
